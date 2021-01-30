67dbe208d4d616a8a5e8818854eccbc8ed3f3234
Sachin Verma <sachinverma53121@gmail.com>
2021-01-31 00:21:27 +0530

    fetch and display

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log("articles: ", articles);
+
   return (
     <div>
       <Head>
         <title>Web Dev News</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to next.js</h1>
+
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=7`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
