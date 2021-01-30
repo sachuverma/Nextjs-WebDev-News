bf127ac548d418eff322b30c608db8e17146b6b4
Sachin Verma <sachinverma53121@gmail.com>
2021-01-30 22:43:32 +0530

    initial pages

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function about() {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <h1>about page</h1>
+    </div>
+  );
+}
+
+export default about;
